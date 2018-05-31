f8fc2d0473af82a8042d2850d1d44a0c3f537487
Zach Whitten <zwhitten@gmail.com>
2018-05-31 19:13:55 -0400

    Sort of working but not 100%

---
+++ lib/db.dart
@@ -0,0 +1,153 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseClient{
+  static final DatabaseClient _instance = new DatabaseClient.internal();
+
+  factory DatabaseClient() => _instance;
+  static Database _db;
+
+  Future<Database> get db async {
+    if(_db!=null) {
+      return _db;
+    }
+    _db = await init();
+    return _db;
+  }
+  DatabaseClient.internal();
+
+
+
+  init() async{
+    Directory path = await getApplicationDocumentsDirectory();
+    String dbString = join(path.path, "queue.db");
+
+    var database = await openDatabase(dbString, version: 1, onCreate: this.initTables);
+    return database;
+  }
+
+  Future initTables(Database db, int version) async{
+    await db.execute('''
+    create table $mediaTable (
+      $columnId integer primary key,
+      $columnOrder integer not null,
+      $columnTitle text not null,
+      $columnType text not null,
+      $columnNotes text default null,
+      $columnComplete integer default 0
+    )
+    ''');
+  }
+
+  Future<Media> insert(Media media) async {
+    var dbClient = await db;
+    media.id = await dbClient.insert(mediaTable, media.toMap());
+    return media;
+  }
+
+  Future<int> updateOrder(int id, int newOrder) async {
+    debugPrint("Updating item_id: $id to new position $newOrder");
+
+    var dbClient = await db;
+    return await dbClient.update(mediaTable, {
+      columnOrder: newOrder
+    },
+    where: "id = ? ",
+    whereArgs: [id]);
+
+  }
+
+  Future<Media> getMedia(int id) async {
+    var dbClient = await db;
+    List<Map> maps = await dbClient.query(mediaTable,
+      columns: [columnId, columnComplete, columnTitle],
+      where: "$columnId = ? ",
+      whereArgs: [id]);
+    if(maps.length > 0) {
+      return Media.fromMap(maps.first);
+    }
+    return null;
+  }
+
+  Future<List<Media>> getMediaByType(String type) async {
+    final result = <Media>[];
+    var dbClient = await db;
+    List<Map> resultList = await dbClient.query(mediaTable,
+      columns: [columnId, columnTitle, columnComplete, columnNotes, columnOrder],
+      where: "$columnType = ? ",
+      whereArgs: [type],
+      orderBy: "$columnOrder asc");
+
+    for(var mapEntry in resultList){
+      result.add(Media.fromMap(mapEntry));
+    }
+    debugPrint("Querying for $type and found "+result.length.toString());
+    return result;
+
+  }
+
+  Future<int> delete(int id) async{
+    var dbClient = await db;
+    return await dbClient.delete(mediaTable, where: "$columnId = ? ", whereArgs: [id]);
+  }
+
+  Future<int> update(Media media) async {
+    var dbClient = await db;
+    return await dbClient.update(mediaTable, media.toMap(),
+      where: "$columnId = ? ", whereArgs: [media.id]);
+  }
+
+//  Future close() async => database.close();
+}
+
+final String mediaTable = "media";
+final String columnId = "id";
+final String columnOrder = "priority";
+final String columnTitle = "title";
+final String columnType="type";
+final String columnNotes="notes";
+final String columnComplete="complete";
+
+class Media {
+  int id;
+  int order;
+  String title;
+  String type;
+  String notes;
+  bool complete;
+  Key key;
+
+  Map<String, dynamic> toMap(){
+    var map = new Map<String, dynamic>();
+
+    map[columnTitle]= title;
+    map[columnType]= type;
+    map[columnNotes]= notes;
+    map[columnOrder]= order;
+    map[columnComplete]= complete == true ? 1 : 0;
+
+    if(id!=null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+
+  Media();
+
+  Media.fromMap(dynamic map){
+    id = map[columnId];
+    order = map[columnOrder];
+    title = map[columnTitle];
+    type= map[columnType];
+    notes=map[columnNotes];
+    complete = map[columnComplete]==1;
+    key = ValueKey(id);
+
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'db.dart';
+import 'reorderable_list.dart';
 
 void main() => runApp(new MyApp());
 
@@ -7,103 +9,298 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Media Queue',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new AppPage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class AppPage extends StatefulWidget
+{
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  AppPageState createState() => new AppPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class AppPageState extends State<AppPage>
+  with SingleTickerProviderStateMixin{
+  DatabaseClient client = new DatabaseClient();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final List<Tab> myTabs = <Tab> [
+    Tab(icon: Icon(Icons.bookmark)),
+    Tab(icon: Icon(Icons.live_tv))
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(length: myTabs.length, vsync: this);
+  }
+
+  @override
+  void dispose(){
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+    return
+         new Scaffold(
+          appBar: new AppBar(
+            bottom: new TabBar(
+              controller: _tabController,
+              tabs: myTabs,
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+            title: new Text("Media Queue"),
+          ),
+          body: new TabBarView(
+              controller: _tabController,
+              children: [
+            new MediaPage(type: 'book'),
+            new MediaPage(type: 'show')
+          ]),
+          floatingActionButton: new FloatingActionButton(
+              onPressed: _newItem, child: new Icon(Icons.add)),
+        );
+  }
+
+  void _newItem() {
+    String type = _tabController.index==0? 'book':'show';
+    Navigator
+        .of(context)
+        .push(new MaterialPageRoute(builder: (BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title:  Text("Add new $type to queue"),
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: new MyForm(type: type)
+      );
+    })).then((val)=> setState((){
+      debugPrint("Set state called...");
+    }));
+  }
+
+  void resetState() {
+    debugPrint("Reset called");
+    setState(() {});
+  }
+}
+
+class MyForm extends StatefulWidget {
+  MyForm({this.type});
+  final String type;
+  @override
+  MyFormState createState() => new MyFormState(type: type);
+}
+
+class MyFormState extends State<MyForm> {
+  MyFormState({this.type});
+  final String type;
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  Media _mediaInfo = new Media();
+  DatabaseClient client = new DatabaseClient();
+  void addMedia() {
+//    setState(() {
+    _mediaInfo.type = this.type;
+    _mediaInfo.order = 100;
+    client.insert(_mediaInfo);
+//    parentReset();
+//    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Form(
+        key: _formKey,
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new TextFormField(
+              decoration: new InputDecoration(
+                  hintText: 'Moby Dick', labelText: 'Book Title'),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please Enter a Title';
+                }
+              },
+              onSaved: (String value) {
+                this._mediaInfo.title = value;
+              },
+            ),
+            new Text('Notes'),
+            new TextFormField(
+              onSaved: (String value) {
+                this._mediaInfo.notes = value;
+              },
+            ),
+            new Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: new RaisedButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      Scaffold.of(context).showSnackBar(
+                          new SnackBar(content: new Text("ProcessingData")));
+                      _formKey.currentState.save();
+                      addMedia();
+                      Navigator.pop(context, true);
+                    }
+                  },
+                  child: new Text(
+                    'Submit',
+                    style: new TextStyle(color: Colors.white),
+                  ),
+                  color: Colors.blue,
+                ))
+          ],
+        ));
+  }
+}
+
+class MediaPage extends StatefulWidget {
+  const MediaPage({this.type});
+  final String type;
+  @override
+  MediaPageState createState() => new MediaPageState(mediaType: this.type);
+}
+
+class MediaPageState extends State<MediaPage> {
+  MediaPageState({this.mediaType});
+  final String mediaType;
+  List<Media> mediaList = <Media>[];
+  DatabaseClient client = new DatabaseClient();
+
+  @override
+  void initState() {
+    debugPrint("initState");
+    client.getMediaByType(this.mediaType).then(setMedia);
+    super.initState();
+  }
+
+  void setMedia(List<Media> list) {
+    setState(() {
+      this.mediaList = list;
+    });
+  }
+
+  @override
+  void dispose() {
+    debugPrint("Dispose");
+    super.dispose();
+  }
+
+  bool _reorderCallBack(Key item, Key newPosition) {
+    int draggingIndex = _indexOfKey(item);
+    int newPositionIndex = _indexOfKey(newPosition);
+
+    final Media draggedItem = mediaList[draggingIndex];
+    setState(() {
+      mediaList.removeAt(draggingIndex);
+      mediaList.insert(newPositionIndex, draggedItem);
+
+      int startIndex = newPositionIndex == 0 ? 0 : newPositionIndex - 1;
+      for (int i = startIndex; i < mediaList.length; ++i) {
+        client.updateOrder(mediaList[i].id, i);
+      }
+      debugPrint(
+          "Reordering " + item.toString() + " -> " + newPosition.toString());
+//      client.getMediaByType("book").then(this.displayBooks);
+    });
+    return true;
+  }
+
+  int _indexOfKey(Key key) {
+    for (int i = 0; i < mediaList.length; ++i) {
+      if (mediaList[i].key == key) return i;
+    }
+    return -1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+            child: ReorderableList(
+                onReorder: this._reorderCallBack,
+                child: ListView.builder(
+                  itemCount: this.mediaList.length,
+                  itemBuilder: (BuildContext c, index) => new Item(
+                      media: mediaList[index],
+                      first: index == 0,
+                      last: index == mediaList.length - 1,
+                      deleteAction: () => this.deleteItem(mediaList[index].id))
+                )
+      )
+      )
+
+      ],
     );
+  }
+
+  void deleteItem(int id) {
+    setState(() {
+      client.delete(id);
+      client.getMediaByType(this.mediaType).then(this.setMedia );
+    });
+  }
+}
+
+class Item extends StatelessWidget {
+  Item({this.media, this.first, this.last, this.deleteAction});
+
+  final Media media;
+  final bool first;
+  final bool last;
+
+  var deleteAction;
+
+  BoxDecoration _buildDecoration(BuildContext context, bool dragging) {
+    return BoxDecoration(
+        border: Border(
+            top: first && !dragging
+                ? Divider.createBorderSide(context)
+                : BorderSide.none,
+            bottom: last && dragging
+                ? BorderSide.none
+                : Divider.createBorderSide(context)));
+  }
+
+  Widget _buildChild(BuildContext context, bool dragging) {
+    return GestureDetector(
+        onLongPress: () => deleteAction(),
+        child: Container(
+          height: 50.0,
+      decoration:
+          BoxDecoration(
+              color: dragging ? Colors.lightGreen : Colors.white70,
+              border: Border.all(
+              color: Colors.lightBlueAccent,
+            width: 1.0
+          )),
+      child: SafeArea(
+          top: false,
+          bottom: false,
+          child: Row(
+            children: <Widget>[
+              Checkbox(value: false),
+              Expanded(child: Text(media.title)),
+              Icon(Icons.reorder, color: dragging ? Colors.blue : Colors.grey)
+            ],
+          )),
+      padding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 14.0),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ReorderableItem(
+        key: media.key,
+        childBuilder: _buildChild,
+        decorationBuilder: _buildDecoration);
   }
 }
